3818cab1a4845bd005b3153ff2eaa94243ab43bf
ngantn1994 <ngantn1994@gmail.com>
2021-08-24 00:03:30 +0900

    [Flutter Course] Hello Flutter (module 1 contents)

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:globo_fitness/screens/intro_screen.dart';
+
+void main() {
+  runApp(GlobeApp());
+}
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: IntroScreen(),
+    );
+  }
+}
